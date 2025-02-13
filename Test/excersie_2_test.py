ba58acf5e0b5352a99005340d1d2e8851089f5f3
lowersde <lowersde@jncb.com>
2025-02-13 18:54:55 -0500

    updated working dir

--- Test/excersie_2_test.py
+++ Test/excersie_2_test.py
@@ -1,5 +1,5 @@
 import unittest
-from excersie2 import substitution_box, inverse_substitution_box, XOR, permutation, inverse_permutation, encrypt, decrypt, brute_force_attack, BLOCK_SIZE, KEY_SIZE
+from Excersie.excersie2 import encrypt, decrypt
 
 ROUNDS = 2
 class TestCipher(unittest.TestCase):
